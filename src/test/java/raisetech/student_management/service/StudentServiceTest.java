8057ed8f417044294446a208d59f6d88a75cb0b1
Kentaro Hirose <hirotaroken@gmail.com>
2025-11-05 23:52:25 +0900

    テストのコードを修正しました

--- src/test/java/raisetech/student_management/service/StudentServiceTest.java
+++ src/test/java/raisetech/student_management/service/StudentServiceTest.java
@@ -62,8 +62,8 @@ class StudentServiceTest {
   @ParameterizedTest
   @CsvSource({"1", "2", "3"})
   void 受講生詳細の検索＿リポジトリが適切に呼び出されていること(String id){
-
     Student student = new Student();
+    student.setId(id);
     List<StudentCourse> studentCourse = new ArrayList<>();
     when(repository.searchStudent(id)).thenReturn(student);
     when(repository.searchStudentCourse(student.getId())).thenReturn(studentCourse);
@@ -80,30 +80,15 @@ class StudentServiceTest {
   void 受講生詳細の登録＿入力した値が適切にオブジェクトに入ること(){
     StudentDetail studentDetail = new StudentDetail();
     Student student = new Student();
-    student.setId("1");
-    student.setFullName("山田　太郎");
-    student.setFurigana("ヤマダタロウ");
-    student.setHandleName("タロー");
-    student.setMailAddress("taro@example.com");
-    student.setArea("東京");
-    student.setAge(30);
-    student.setGender("男性");
-    student.setRemark("");
-    student.isDeleted();
     studentDetail.setStudent(student);
-
-
     StudentCourse studentCourse = new StudentCourse();
     List<StudentCourse> studentCourseList = new ArrayList<>();
-    studentCourse.setStudentId("1");
-    studentCourse.setStudentId("1");
-    studentCourse.setCourseName("国語");
-    studentCourse.setStartDate(LocalDateTime.now());
-    studentCourse.setCompleteDate(LocalDateTime.now().plusYears(1));
     studentCourseList.add(studentCourse);
     studentDetail.setStudentCourseList(studentCourseList);
+    doNothing().when(repository).registerStudent(student);
+    doNothing().when(repository).registerStudentCourse(studentCourse);
 
-    StudentDetail actual = sut.registerNewStudent(studentDetail);
+    sut.registerNewStudent(studentDetail);
 
     verify(repository, times(1)).registerStudent(student);
     verify(repository, times(1)).registerStudentCourse(studentCourse);
@@ -114,36 +99,19 @@ class StudentServiceTest {
   void 受講生詳細の更新＿入力した値で適切に更新が機能すること(){
     StudentDetail studentDetail = new StudentDetail();
     Student student = new Student();
-    student.setId("1");
-    student.setFullName("山田　太郎");
-    student.setFurigana("ヤマダタロウ");
-    student.setHandleName("タロー");
-    student.setMailAddress("taro@example.com");
-    student.setArea("東京");
-    student.setAge(30);
-    student.setGender("男性");
-    student.setRemark("");
-    student.isDeleted();
     studentDetail.setStudent(student);
-
-
-    StudentCourse studentCourse = new StudentCourse();
     List<StudentCourse> studentCourseList = new ArrayList<>();
-    studentCourse.setStudentId("1");
-    studentCourse.setStudentId("1");
-    studentCourse.setCourseName("国語");
-    studentCourse.setStartDate(LocalDateTime.now());
-    studentCourse.setCompleteDate(LocalDateTime.now().plusYears(1));
+    StudentCourse studentCourse = new StudentCourse();
     studentCourseList.add(studentCourse);
     studentDetail.setStudentCourseList(studentCourseList);
-
-    repository.updateStudent(student);
-    repository.updateStudentCourse(studentCourse);
-
+    doNothing().when(repository).updateStudent(student);
+    doNothing().when(repository).updateStudentCourse(studentCourse);
 
 
-    verify(repository, times(1)).updateStudent(student);
-    verify(repository, times(1)).updateStudentCourse(studentCourse);
+     sut.updateStudent(studentDetail);
 
+     verify(repository, times(1)).updateStudent(student);
+     verify(repository, times(1)).updateStudentCourse(studentCourse);
+//
   }
 }
