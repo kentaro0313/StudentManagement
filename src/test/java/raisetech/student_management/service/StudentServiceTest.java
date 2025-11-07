52f6cab303b43f44190184729cf6aad5a7fe29cd
Kentaro Hirose <hirotaroken@gmail.com>
2025-11-07 15:31:54 +0900

    不要な空白を削除しました

--- src/test/java/raisetech/student_management/service/StudentServiceTest.java
+++ src/test/java/raisetech/student_management/service/StudentServiceTest.java
@@ -1,26 +1,17 @@
 package raisetech.student_management.service;
 
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-
 import java.util.ArrayList;
 import java.util.List;
-
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-
-
-
-import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import raisetech.student_management.controller.converter.StudentConverter;
@@ -48,7 +39,6 @@ class StudentServiceTest {
 
   @Test
   void 受講生詳細の一覧検索＿リポジトリとコンバーターの処理が適切に呼び出せていること() {
-
     List<Student> studentList = new ArrayList<>();
     List<StudentCourse> studentCourseList = new ArrayList<>();
     when(repository.search()).thenReturn(studentList);
@@ -62,14 +52,14 @@ class StudentServiceTest {
 
   }
 
- @Test
+  @Test
   void 受講生詳細の検索＿リポジトリが適切に呼び出されていること() {
     Student student = new Student();
     student.setId("999");
+    student.setFullName("田中　太郎");
     StudentCourse studentCourse = new StudentCourse();
     List<StudentCourse> studentCourseList = List.of(studentCourse);
     StudentDetail studentDetail = new StudentDetail(student, studentCourseList);
-
     when(repository.searchStudent(student.getId())).thenReturn(student);
     when(repository.searchStudentCourse(student.getId())).thenReturn(studentCourseList);
 
@@ -80,17 +70,19 @@ class StudentServiceTest {
     Assertions.assertNotNull(actual.getStudent(), "NULLになってます");
     Assertions.assertNotNull(actual.getStudentCourseList(), "NULLになってます");
     assertThat(actual.getStudent().getId()).isEqualTo(studentDetail.getStudent().getId());
-    assertThat(actual.getStudent().getFullName()).isEqualTo(studentDetail.getStudent().getFullName());
+    assertThat(actual.getStudent().getFullName()).isEqualTo(
+        studentDetail.getStudent().getFullName());
   }
 
 
   @Test
   void 受講生詳細の登録＿リポジトリが適切に呼び出されていること() {
     Student student = new Student();
+    student.setId("999");
+    student.setFullName("田中　太郎");
     StudentCourse studentCourse = new StudentCourse();
     List<StudentCourse> studentCourseList = List.of(studentCourse);
     StudentDetail studentDetail = new StudentDetail(student, studentCourseList);
-
     doNothing().when(repository).registerStudent(student);
     doNothing().when(repository).registerStudentCourse(studentCourse);
 
@@ -101,7 +93,8 @@ class StudentServiceTest {
     Assertions.assertNotNull(actual.getStudent(), "NULLになってます");
     Assertions.assertNotNull(actual.getStudentCourseList(), "NULLになってます");
     assertThat(actual.getStudent().getId()).isEqualTo(studentDetail.getStudent().getId());
-    assertThat(actual.getStudent().getFullName()).isEqualTo(studentDetail.getStudent().getFullName());
+    assertThat(actual.getStudent().getFullName()).isEqualTo(
+        studentDetail.getStudent().getFullName());
 
   }
 
@@ -111,7 +104,6 @@ class StudentServiceTest {
     StudentCourse studentCourse = new StudentCourse();
     List<StudentCourse> studentCourseList = List.of(studentCourse);
     StudentDetail studentDetail = new StudentDetail(student, studentCourseList);
-
     doNothing().when(repository).updateStudent(student);
     doNothing().when(repository).updateStudentCourse(studentCourse);
 
